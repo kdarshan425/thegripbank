c6211f2dfe7768bafff061788e674d6f8f500d1c
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-08-11 16:36:53 +0530

    updated db

--- bank/db.php
+++ bank/db.php
@@ -1,9 +1,9 @@
 <?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "bank";
+$servername = "remotemysql.com";
+$username = "txiLScJh13";
+$password = "rnqjldz5Gt";
+$database = "txiLScJh13";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die(mysqli_error($conn));
 
