444363425c1550dd766d738d0567e0731b7c2262
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-10-06 20:30:21 +0530

    Updated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div style="padding-top:80px;">
 <footer class="container" style="background: black;padding-top: 24px;padding-bottom: 24px; width: 100%">
     <center>
-        <p style="color: white;font-weight: 300;font-family: 'Raleway', sans-serif;"> Created by Darshan Khope<br>Kindly mail at kdarshan425@gmail.com<br>task 1 Web developement internship<br>The sparks foundation</p>
+        <p style="color: white;font-weight: 300;font-family: 'Raleway', sans-serif;"> Created by <br> Darshan Khope, Vishal khandagale, Arnav kulkarni, shubham joshi</p>
     </center>            
 </footer>
 </div>
