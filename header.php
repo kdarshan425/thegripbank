8c116bec8ffe1d6503e0ff6a7646016aaa41f0c4
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-11-11 22:00:49 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,13 @@
 <nav style="background: black">
     <div class="topnav" id="myTopnav">
         <a href="index.php" style="float:left;font-weight: 900;font-family: 'Raleway', sans-serif;">THE GRIP BANK</a>
-        <a style="height: 60px;"></a>  
-        <a  href="task.php">task</a>                  
+        <a style="height: 60px;"></a>                         
         <a  href="users.php">users</a>
         <a  href="transaction.history.php">History</a>
-        <a  href="index.php">Home</a>       
-
+        <a  href="index.php">Home</a>  
         <a href="javascript:void(0);" class="icon"  onclick="dk_open()">
-           lll <i class="fa fa-bars"></i>
-        </a>
-    
+           ||| <i class="fa fa-bars"></i>
+        </a>    
     </div>
 </nav>
 
@@ -21,6 +18,4 @@
     <a  href="index.php" onclick="dk_close()">Home</a>      
     <a  href="users.php" onclick="dk_close()">users</a>
     <a  href="transaction.history.php" onclick="dk_close()">History</a>
-    <a  href="task.php" onclick="dk_close()">task</a>
-    
 </nav>
